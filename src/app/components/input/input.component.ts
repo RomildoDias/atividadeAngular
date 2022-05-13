0e0d322fc52168c2704d1790221f069b90424b02
RomildoDias <rdc.dias.c@gmail.com>
2022-05-13 09:34:10 -0300

    criado o formulário

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
